3af78aaa0c9691ab14f13de4db172ea3c97dc47a
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2020-02-10 20:35:52 +0900

    Add root model and translator

---
+++ ChukyoBustime/Application/Model/Objects/PdfUrl.swift
@@ -0,0 +1,14 @@
+//
+//  PdfUrl.swift
+//  ChukyoBustime
+//
+//  Created by 山田隼也 on 2020/02/10.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+struct PdfUrl {
+    let calendar: String
+    let timeTable: String
+}
---
+++ ChukyoBustime/Application/Model/Root/RootModel.swift
@@ -0,0 +1,40 @@
+//
+//  RootModel.swift
+//  ChukyoBustime
+//
+//  Created by 山田隼也 on 2020/02/10.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+import Infra
+import RxSwift
+
+// MARK: - Interface
+
+protocol RootModel: AnyObject {
+    func getPdfUrl() -> Single<PdfUrl>
+}
+
+// MARK: - Implementation
+
+class RootModelImpl: RootModel {
+    
+    // MARK: Dependency
+    
+    private let remoteConfigProvider: RemoteConfigProvider
+    
+    // MARK: Initializer
+    
+    init(remoteConfigProvider: RemoteConfigProvider = RemoteConfigProvider.shared) {
+        self.remoteConfigProvider = remoteConfigProvider
+    }
+    
+    // MARK: Remote Config
+    
+    func getPdfUrl() -> Single<PdfUrl> {
+        return remoteConfigProvider
+            .getConfigValue(for: .pdfUrl, configType: RCPdfUrlEntity.self)
+            .translate(PdfUrlTranslator())
+    }
+}
