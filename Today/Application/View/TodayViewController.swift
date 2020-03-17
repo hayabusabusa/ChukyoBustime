e35081b3ca7ff966e44d74919899caf228287ef5
hayabusabusa <hayabusabusaSY@gmail.com>
2020-03-17 18:00:09 +0900

    Update layout and add url scheme

--- Today/Application/View/TodayViewController.swift
+++ Today/Application/View/TodayViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
-import Infra
 import NotificationCenter
 
-class TodayViewController: UIViewController, NCWidgetProviding {
+final class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    // MARK: IBOutlet
+    
+    @IBOutlet private weak var parentButton: UIButton!
+    @IBOutlet private weak var parentStackView: UIStackView!
+    @IBOutlet private weak var collegeTimeLabel: UILabel!
+    @IBOutlet private weak var stationTimeLabel: UILabel!
+    @IBOutlet private weak var lineView: UIView!
+    @IBOutlet private weak var messageLabel: UILabel!
+    
+    // MARK: Lifecycle
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupParentStackViews()
+        setupMessageLabel()
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -27,4 +38,40 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         completionHandler(NCUpdateResult.newData)
     }
     
+    // MARK: Tap event
+    
+    @objc
+    private func onTapParentButton(_ sender: UIButton) {
+        guard let url = URL(string: Configurations.kApplicationUrlScheme) else { return }
+        extensionContext?.open(url, completionHandler: nil)
+    }
+    
+}
+
+// MARK: - Setup
+
+extension TodayViewController {
+    
+    private func setupParentStackViews() {
+        parentButton.addTarget(self, action: #selector(onTapParentButton(_:)), for: .touchUpInside)
+        parentStackView.alpha = 0
+        lineView.alpha = 0
+    }
+    
+    private func setupMessageLabel() {
+        messageLabel.text = "データがありません。\nアプリを起動してデータを取得してください。"
+        //messageLabel.alpha = 0
+    }
+}
+
+// MARK: - Animation
+
+extension TodayViewController {
+    
+    private func showParentStackViewAnimation() {
+        UIView.animate(withDuration: 0.5) {
+            self.parentStackView.alpha = 1
+            self.lineView.alpha = 1
+        }
+    }
 }
