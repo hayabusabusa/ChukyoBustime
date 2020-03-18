1b3d118506a3b5109bafc14249d734b5e1b1c476
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2020-03-19 00:49:59 +0900

    Fix xcode settings

--- Infra/Configs/Configurations.swift
+++ Infra/Configs/Configurations.swift
@@ -11,5 +11,5 @@ import Foundation
 enum Configurations {
     // TODO: Setup app group.
     /// AppGroup で設定した共有コンテナのURL
-    static let kAppGraoupURL: URL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "")!
+    static let kAppGraoupURL: URL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.jp.shunya.yamada.ChukyoBustime")!
 }
