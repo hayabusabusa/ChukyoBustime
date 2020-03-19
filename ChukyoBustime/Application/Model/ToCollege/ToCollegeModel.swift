1cbbf9f5a2bea9bbe059ad094ed5f2c76016d282
hayabusabusa <hayabusabusaSY@gmail.com>
2020-03-19 11:13:42 +0900

    Remove cache function

--- ChukyoBustime/Application/Model/ToCollege/ToCollegeModel.swift
+++ ChukyoBustime/Application/Model/ToCollege/ToCollegeModel.swift
@@ -35,22 +35,9 @@ class ToCollegeModelImpl: ToCollegeModel {
     
     // MARK: Firestore
     
-    /// 時刻表のデータをキャッシュもしくはFirestoreから取得する.
+    /// 時刻表のデータをFirestoreから取得する.
     /// - Parameter date: 取得するタイミングの`Date`.
     func getBusTimes(at date: Date) -> Single<(busDate: BusDate, busTimes: [BusTime])> {
-        return localCacheRepository.checkCache(at: date, destination: .toCollege)
-            .flatMap {
-                $0 ? self.getBusTimesFromCache(at: date) : self.getBusTimesFromFirestore(at: date)
-            }
-    }
-    
-    private func getBusTimesFromCache(at date: Date) -> Single<(busDate: BusDate, busTimes: [BusTime])> {
-        return localCacheRepository.loadCache(of: CollegeLocalCache.self)
-            .map { (busDate: $0.busDate ?? BusDateEntity(diagram: "UNKNOWN", diagramName: "UNKNOWN"), busTimes: Array($0.busTimes)) }
-            .translate(BusDateAndBusTimesTranslator()).debug()
-    }
-    
-    private func getBusTimesFromFirestore(at date: Date) -> Single<(busDate: BusDate, busTimes: [BusTime])> {
         return firestoreRepository.getBusTimes(at: date, destination: .toCollege)
             .flatMap { value -> Single<(busDate: BusDate, busTimes: [BusTime])> in
                 // NOTE: キャッシュを保存して完了した後にFirestoreから取得したものを流す.
