15cf3df2cb81380d2a23f22b4cdcbd0d0e31d32e
hayabusabusa <hayabusabusaSY@gmail.com>
2020-09-11 22:05:15 +0900

    Add infra unit test target

---
+++ InfraTests/InfraTests.swift
@@ -0,0 +1,33 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by 山田隼也 on 2020/09/11.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import XCTest
+
+class InfraTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
