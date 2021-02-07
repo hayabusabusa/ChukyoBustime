f973ff2ebfdac7509a3147d2cb344e48503e524c
hayabusabusa <hayabusabusaSY@gmail.com>
2021-02-07 22:33:55 +0900

    chore: add test target configuration file

--- ChukyoBustimeTests/ChukyoBustimeTests.swift
+++ ChukyoBustimeTests/ChukyoBustimeTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import ChukyoBustime
 
 class ChukyoBustimeTests: XCTestCase {
 
@@ -19,8 +20,6 @@ class ChukyoBustimeTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
 }
