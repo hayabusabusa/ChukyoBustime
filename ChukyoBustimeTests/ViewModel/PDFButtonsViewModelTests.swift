2f363f2a3de994eecbd77d1f3642619cd5d3a4ee
hayabusabusa <hayabusabusaSY@gmail.com>
2021-02-25 18:22:20 +0900

    test: add PDF buttons view model tests

---
+++ ChukyoBustimeTests/ViewModel/PDFButtonsViewModelTests.swift
@@ -0,0 +1,69 @@
+//
+//  PDFButtonsViewModelTests.swift
+//  ChukyoBustimeTests
+//
+//  Created by Shunya Yamada on 2021/02/24.
+//  Copyright © 2021 Shunya Yamada. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import RxTest
+@testable import ChukyoBustime
+
+class PDFButtonsViewModelTests: XCTestCase {
+
+    func test_カレンダーボタンを押した際にURLが流れてくることを確認() {
+        let url = URL(string: "http://example.com")!
+        let model = MockPDFButtonsModelImpl()
+        let viewModel = PdfButtonsViewModel(model: model)
+        let disposeBag = DisposeBag()
+        
+        let scheduler = TestScheduler(initialClock: 0)
+        let testableObserver = scheduler.createObserver(URL.self)
+        
+        scheduler.scheduleAt(100) {
+            viewModel.output.presentSafari
+                .emit(to: testableObserver)
+                .disposed(by: disposeBag)
+        }
+        
+        scheduler.scheduleAt(200) {
+            viewModel.input.tappedCalendarButton()
+        }
+        
+        scheduler.start()
+        
+        let expression = Recorded.events([
+            .next(200, url)
+        ])
+        XCTAssertEqual(testableObserver.events, expression)
+    }
+    
+    func test_時刻表ボタンを押した際にURLが流れてくることを確認() {
+        let url = URL(string: "http://example.org")!
+        let model = MockPDFButtonsModelImpl()
+        let viewModel = PdfButtonsViewModel(model: model)
+        let disposeBag = DisposeBag()
+        
+        let scheduler = TestScheduler(initialClock: 0)
+        let testableObserver = scheduler.createObserver(URL.self)
+        
+        scheduler.scheduleAt(100) {
+            viewModel.output.presentSafari
+                .emit(to: testableObserver)
+                .disposed(by: disposeBag)
+        }
+        
+        scheduler.scheduleAt(200) {
+            viewModel.input.tappedTimeTableButton()
+        }
+        
+        scheduler.start()
+        
+        let expression = Recorded.events([
+            .next(200, url)
+        ])
+        XCTAssertEqual(testableObserver.events, expression)
+    }
+}
