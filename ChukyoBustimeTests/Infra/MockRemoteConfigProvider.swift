49786544e9076feacf2f04f1a46c160b1fa7126a
hayabusabusa <hayabusabusaSY@gmail.com>
2021-03-13 15:29:40 +0900

    test: add mock remote config provider

---
+++ ChukyoBustimeTests/Infra/MockRemoteConfigProvider.swift
@@ -0,0 +1,14 @@
+//
+//  MockRemoteConfigProvider.swift
+//  ChukyoBustimeTests
+//
+//  Created by Shunya Yamada on 2021/03/13.
+//  Copyright © 2021 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+import Infra
+
+final class MockRemoteConfigProvider {
+    
+}
