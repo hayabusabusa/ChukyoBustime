80a2475544fb39da636a183b61edf59d851844de
hayabusabusa <hayabusabusaSY@gmail.com>
2021-03-14 17:43:30 +0900

    test: fix mock bus list model impl

--- ChukyoBustimeTests/Model/Mock/MockBusListModelImpl.swift
+++ ChukyoBustimeTests/Model/Mock/MockBusListModelImpl.swift
@@ -7,34 +7,36 @@
 //
 
 import RxSwift
+import RxRelay
 @testable import ChukyoBustime
 
 final class MockBusListModelImpl: BusListModel {
-    let isAuthorized: Bool
-    let isErrorOccured: Bool
+    private let isAuthorized: Bool
+    private let isErrorOccured: Bool
+    
+    private let errorRelay: PublishRelay<String>
+    let errorStream: Observable<String>
+    
+    private let messageRelay: PublishRelay<String>
+    let messageStream: Observable<String>
+    
     
     init(isAuthorized: Bool = true, isErrorOccured: Bool = false) {
         self.isAuthorized = isAuthorized
         self.isErrorOccured = isErrorOccured
+        self.errorRelay = .init()
+        self.messageRelay = .init()
+        
+        errorStream = errorRelay.asObservable()
+        messageStream = messageRelay.asObservable()
     }
     
-    func requestAuthorization() -> Completable {
-        return Completable.create { observer in
-            self.isAuthorized
-                ? observer(.completed)
-                : observer(.error(MockError.somethingWentWrong))
-            
-            return Disposables.create()
+    func setNotification(at busTime: BusTime) {
+        if !isAuthorized || isErrorOccured {
+            errorRelay.accept(MockError.somethingWentWrong.description)
+            return
         }
-    }
-    
-    func setNotification(at busTime: BusTime) -> Completable {
-        return Completable.create { observer in
-            self.isErrorOccured
-                ? observer(.error(MockError.somethingWentWrong))
-                : observer(.completed)
-            
-            return Disposables.create()
-        }
+        
+        messageRelay.accept(String(format: "%02i:%02i の5分前に\n通知が来るように設定しました。", busTime.hour, busTime.minute))
     }
 }
