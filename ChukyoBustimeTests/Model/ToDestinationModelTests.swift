79095ef9c6153ef54ecf25fbdc64f0079ada4325
hayabusabusa <hayabusabusaSY@gmail.com>
2021-12-18 18:33:22 +0900

    test: 汎用 Model の初期状態を確認するテストを追加

--- ChukyoBustimeTests/Model/ToDestinationModelTests.swift
+++ ChukyoBustimeTests/Model/ToDestinationModelTests.swift
@@ -15,7 +15,51 @@ import XCTest
 
 class ToDestinationModelTests: XCTestCase {
     
-    func test_() {
+    func test_初期状態が正しいことを確認() {
+        let disposeBag = DisposeBag()
         
+        let busDate = BusDateEntity(diagram: "TEST", diagramName: "TEST")
+        let firestoreRepository = MockFirestoreRepositoryImpl(busDate: busDate, busTimes: [])
+        let localCacheRepository = MockLocalCacheRepositoryImpl()
+        let remoteConfigProvider = MockRemoteConfigProvider()
+        let model = ToDestinationModelImpl(for: .toStation,
+                                           firestoreRepository: firestoreRepository,
+                                           localCacheRepository: localCacheRepository,
+                                           remoteConfigProvider: remoteConfigProvider)
+        
+        XCTContext.runActivity(named: "ロード中のフラグは True、エラーは nil で、データには空配列が流れてくること") { _ in
+            let scheduler = TestScheduler(initialClock: 0)
+            let isLoadingTestableObserver = scheduler.createObserver(Bool.self)
+            let busTimesTestableObserver = scheduler.createObserver(Int.self)
+            let errorTestableObserver = scheduler.createObserver(NSError?.self)
+            
+            model.isLoadingStream
+                .subscribe(isLoadingTestableObserver)
+                .disposed(by: disposeBag)
+            model.busTimesStream
+                .map { $0.count }
+                .subscribe(busTimesTestableObserver)
+                .disposed(by: disposeBag)
+            model.errorStream
+                .map { $0 != nil ? $0! as NSError : nil }
+                .subscribe(errorTestableObserver)
+                .disposed(by: disposeBag)
+            
+            scheduler.start()
+            
+            let expectedIsLoadingEvents = Recorded.events([
+                .next(0, true)
+            ])
+            let expectedBusTimesEvents = Recorded.events([
+                .next(0, 0)
+            ])
+            let expectedErrorEvents: [Recorded<Event<NSError?>>] = Recorded.events([
+                .next(0, nil)
+            ])
+            
+            XCTAssertEqual(isLoadingTestableObserver.events, expectedIsLoadingEvents)
+            XCTAssertEqual(busTimesTestableObserver.events, expectedBusTimesEvents)
+            XCTAssertEqual(errorTestableObserver.events, expectedErrorEvents)
+        }
     }
 }
