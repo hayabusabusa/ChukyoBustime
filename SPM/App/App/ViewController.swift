16b5f6153ca466095827da5fae6b9efedea997c2
hayabusabusa <hayabusabusaSY@gmail.com>
2022-11-02 23:52:36 +0900

    chore: Swift Package Manager 移行用のプロジェクトを作成
    パッケージとアプリ側のプロジェクトを作成。

---
+++ SPM/App/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Shunya Yamada on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
