ea3a43c6b0872ffa9e13cc53208d882665c80c66
hayabusabusa <hayabusabusaSY@gmail.com>
2023-03-05 22:38:20 +0900

    feat: 通知を設定する処理をまとめたサービスクラスを追加

--- SPM/Package/Sources/Service/DateService.swift
+++ SPM/Package/Sources/Service/DateService.swift
@@ -12,13 +12,20 @@ public protocol DateServiceProtocol {
     /// 現在の時刻を 00:00 から経過した秒数に直したもの.
     var nowTimeForSecond: Int { get }
 
-    /// 現在の日付から取り出した `DateComponents` を返す.
-    var nowDateComponents: DateComponents { get }
+    /// ロケールに応じた現在の日付を返す.
+    var nowDate: Date { get }
 
     /// 指定された日付が今日の日付かどうかを返す.
     /// - Parameter date: 任意の日付.
     /// - Returns: 今日の日付かどうか.
     func isToday(_ date: Date) -> Bool
+
+    /// 現在の年月日で、時間と分を指定した日付を返す.
+    /// - Parameters:
+    ///   - hour: 時間.
+    ///   - minute: 分.
+    /// - Returns: 時間と分を指定した `Date`.
+    func today(hour: Int, minute: Int) -> Date
 
     /// 指定された日付をフォーマットした文字列にして返す.
     /// - Parameters:
@@ -40,13 +47,23 @@ public struct DateService: DateServiceProtocol {
         return now.hour * 3600 + now.minute * 60 + now.second
     }
 
-    public var nowDateComponents: DateComponents {
-        now.dateComponents
+    public var nowDate: Date {
+        now.date
     }
 
     public func isToday(_ date: Date) -> Bool {
         let dateInRegion = DateInRegion(date, region: .current)
         return dateInRegion.isToday
+    }
+
+    public func today(hour: Int, minute: Int) -> Date {
+        let now = self.now
+        let dateInRegion = DateInRegion(year: now.year,
+                                        month: now.month,
+                                        day: now.day,
+                                        hour: hour,
+                                        minute: minute)
+        return dateInRegion.date
     }
 
     public func formatted(_ date: Date, format: String) -> String {
---
+++ SPM/Package/Sources/Service/UserNotificationService.swift
@@ -0,0 +1,76 @@
+//
+//  UserNotificationService.swift
+//  
+//
+//  Created by Shunya Yamada on 2023/03/05.
+//
+
+import Foundation
+import UserNotifications
+
+public protocol UserNotificationServiceProtocol {
+    /// 通知の利用の許諾をリクエストする.
+    func authorize() async throws
+
+    /// 通知を追加する.
+    /// - Parameters:
+    ///   - hour: 追加する通知の時間.
+    ///   - minute: 追加する通知の分.
+    func addNotification(hour: Int, minute: Int) async throws
+}
+
+public enum UserNotificationError: Error {
+    /// 通知の利用が許可されていない場合のエラー.
+    case notAuthorized
+    /// 通知が追加可能な 5 分前を過ぎている場合のエラー.
+    case overFiveMinutes
+}
+
+public final class UserNotificationService: UserNotificationServiceProtocol {
+    /// シングルトン.
+    public static let shared = UserNotificationService()
+
+    private let dateService: DateServiceProtocol
+    private let notificationCenter = UNUserNotificationCenter.current()
+
+    init(dateService: DateServiceProtocol = DateService()) {
+        self.dateService = dateService
+    }
+
+    public func authorize() async throws {
+        let settings = await notificationCenter.notificationSettings()
+        switch settings.authorizationStatus {
+        case .authorized:
+            return
+        case .notDetermined:
+            let result = try await notificationCenter.requestAuthorization()
+            if result {
+                return
+            } else {
+                throw UserNotificationError.notAuthorized
+            }
+        default:
+            throw UserNotificationError.notAuthorized
+        }
+    }
+
+    public func addNotification(hour: Int, minute: Int) async throws {
+        let now = dateService.nowDate
+        let requestingDate = dateService.today(hour: hour, minute: minute)
+        // 通知を追加する時刻の 5 分前と今を比較して、すでに時間を過ぎていたらエラーを流す.
+        let requestingDateFiveMinutesAgo = requestingDate.addingTimeInterval(-5 * 60)
+        guard now <= requestingDateFiveMinutesAgo else {
+            throw UserNotificationError.overFiveMinutes
+        }
+
+        let content = UNMutableNotificationContent()
+        content.title = ""
+        content.body = String(format: "🚍 もうすぐ %02i:%02i 発のバスが出発します。", hour, minute)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: requestingDate.dateComponents,
+                                                    repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString,
+                                            content: content,
+                                            trigger: trigger)
+        try await notificationCenter.add(request)
+    }
+}
