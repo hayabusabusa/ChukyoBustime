bdc4b87554d316ad9e0dbb488fbeac936b7a1349
hayabusabusa <hayabusabusaSY@gmail.com>
2023-03-05 21:34:18 +0900

    chore: プロジェクト作成時の起動画面と設定を削除

--- SPM/App/App/SceneDelegate.swift
+++ SPM/App/App/SceneDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Shunya Yamada on 2022/11/02.
 //
 
+import AppFeature
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -13,10 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = Window(windowScene: windowScene)
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
