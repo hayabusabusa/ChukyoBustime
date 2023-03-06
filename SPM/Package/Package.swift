b035b816ffe4268708277f6ae2464b037ab83e7b
hayabusabusa <hayabusabusaSY@gmail.com>
2023-03-07 01:41:28 +0900

    chore: dependency を補完が出るように static な定数で定義

--- SPM/Package/Package.swift
+++ SPM/Package/Package.swift
@@ -3,18 +3,33 @@
 
 import PackageDescription
 
+private extension Target.Dependency {
+    static let appFeature = Target.Dependency(stringLiteral: "AppFeature")
+    static let firestore = Target.Dependency.product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
+    static let firestoreSwift = Target.Dependency.product(name: "FirebaseFirestoreSwift", package: "firebase-ios-sdk")
+    static let remoteConfig = Target.Dependency.product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk")
+    static let remoteConfigSwift = Target.Dependency.product(name: "FirebaseRemoteConfigSwift", package: "firebase-ios-sdk")
+    static let service = Target.Dependency(stringLiteral: "Service")
+    static let serviceProtocol = Target.Dependency(stringLiteral: "ServiceProtocol")
+    static let shared = Target.Dependency(stringLiteral: "Shared")
+    static let swiftDate = Target.Dependency.product(name: "SwiftDate", package: "SwiftDate")
+    static let toDestinationFeature = Target.Dependency(stringLiteral: "ToDestinationFeature")
+}
+
 let package = Package(
     name: "Package",
     platforms: [.iOS(.v14)],
     products: [
-        .library(name: "AppFeature",
-                 targets: [
-                    "AppFeature"
-                 ]),
-        .library(name: "ToDestinationFeature",
-                 targets: [
-                    "ToDestinationFeature"
-                 ])
+        .library(
+            name: "AppFeature",
+            targets: [
+                "AppFeature"
+            ]),
+        .library(
+            name: "ToDestinationFeature",
+            targets: [
+                "ToDestinationFeature"
+            ]),
     ],
     dependencies: [
         .package(
@@ -29,29 +44,32 @@ let package = Package(
         .target(
             name: "AppFeature",
             dependencies: [
-                "ToDestinationFeature",
+                .service,
+                .toDestinationFeature,
             ],
             path: "./Sources/Feature/AppFeature"),
-        .target(name: "ToDestinationFeature",
-                dependencies: [
-                    "ServiceProtocol",
-                    "Shared",
-                ],
-                path: "./Sources/Feature/ToDestinationFeature"),
+        .target(
+            name: "ToDestinationFeature",
+            dependencies: [
+                .serviceProtocol,
+                .shared,
+            ],
+            path: "./Sources/Feature/ToDestinationFeature"),
 
         // MARK: Core
         .target(
             name: "Service",
             dependencies: [
-                "ServiceProtocol",
-                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseFirestoreSwift", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseRemoteConfigSwift", package: "firebase-ios-sdk"),
-                .product(name: "SwiftDate", package: "SwiftDate"),
+                .serviceProtocol,
+                .firestore,
+                .firestoreSwift,
+                .remoteConfig,
+                .remoteConfigSwift,
+                .swiftDate,
             ]),
-        .target(name: "ServiceProtocol",
-                dependencies: []),
+        .target(
+            name: "ServiceProtocol",
+            dependencies: []),
         .target(
             name: "Shared",
             dependencies: []),
@@ -60,7 +78,7 @@ let package = Package(
         .testTarget(
             name: "PackageTests",
             dependencies: [
-                "Service"
+                .service,
             ]),
     ]
 )
