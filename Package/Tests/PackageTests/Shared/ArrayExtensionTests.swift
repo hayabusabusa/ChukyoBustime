e74b8e3df84f68484ebab60eda32ea96b814a1b2
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-01 12:28:54 +0900

    fix: テストフレームワークを Swift Testing に変更
    CI で Xcode のバージョンを 16.1 に変更したので、試しに Swift Testing が利用できるか検証。

--- Package/Tests/PackageTests/Shared/ArrayExtensionTests.swift
+++ Package/Tests/PackageTests/Shared/ArrayExtensionTests.swift
@@ -5,37 +5,32 @@
 //  Created by Shunya Yamada on 2024/12/31.
 //
 
-import XCTest
+import Testing
 @testable import Shared
 
-final class ArrayExtensionTests: XCTestCase {
-    func testPopFirst() {
-        XCTContext.runActivity(named: "配列から削除した要素を返すこと.") { _ in
-            var array = [1, 2, 3]
+struct ArrayExtensionTests {
+    @Test
+    func popFirst() async throws {
+        var array1 = [1, 2, 3]
 
-            let element = array.popFirst()
-            XCTAssertEqual(element, 1)
-        }
+        let element1 = array1.popFirst()
+        #expect(element1 == 1)
 
-        XCTContext.runActivity(named: "配列が空の場合は nil を返すこと.") { _ in
-            var array: [Int] = []
+        var array2: [Int] = []
 
-            let element = array.popFirst()
-            XCTAssertNil(element)
-        }
+        let element2 = array2.popFirst()
+        #expect(element2 == nil)
 
-        XCTContext.runActivity(named: "配列から全ての要素を削除できること.") { _ in
-            var array = [1, 2, 3]
+        var array3 = [1, 2, 3]
 
-            let firstElement = array.popFirst()
-            let secondElement = array.popFirst()
-            let thirdElement = array.popFirst()
-            let fourthElement = array.popFirst()
+        let firstElement = array3.popFirst()
+        let secondElement = array3.popFirst()
+        let thirdElement = array3.popFirst()
+        let fourthElement = array3.popFirst()
 
-            XCTAssertEqual(firstElement, 1)
-            XCTAssertEqual(secondElement, 2)
-            XCTAssertEqual(thirdElement, 3)
-            XCTAssertNil(fourthElement)
-        }
+        #expect(firstElement == 1)
+        #expect(secondElement == 2)
+        #expect(thirdElement == 3)
+        #expect(fourthElement == nil)
     }
 }
