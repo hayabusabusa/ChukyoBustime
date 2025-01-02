11b2081730ed586f20c42eab5b04da2c1c489cea
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-02 11:03:40 +0900

    fix: ダイヤ名表示の View を個別に切り出し

--- Package/Sources/ToDestinationFeature/ToDestinationView.swift
+++ Package/Sources/ToDestinationFeature/ToDestinationView.swift
@@ -343,35 +343,7 @@ public struct ToDestinationView: View {
                     ScrollView {
                         VStack {
                             if let diagramName = store.state.diagramName {
-                                VStack {
-                                    Text("今日の運行ダイヤ")
-                                        .font(.system(size: 14))
-                                        .frame(
-                                            maxWidth: .infinity,
-                                            alignment: .leading
-                                        )
-
-                                    Text(diagramName)
-                                        .font(
-                                            .system(
-                                                size: 32,
-                                                weight: .bold
-                                            )
-                                        )
-                                        .foregroundStyle(.blue)
-                                }
-                                .frame(maxWidth: .infinity)
-                                .padding(
-                                    EdgeInsets(
-                                        top: 24,
-                                        leading: 16,
-                                        bottom: 24,
-                                        trailing: 16
-                                    )
-                                )
-                                .background(
-                                    Color(.secondarySystemGroupedBackground)
-                                )
+                                DiagramNameView(diagramName: diagramName)
                             }
 
                             CountdownView(
